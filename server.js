2458df902e558dc4b6ddbd885b5d62e447102e97
ZavodPodushek <new_mouse@mail.ru>
2018-06-10 16:55:53 +0300

    Fix route system

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist'));
-app.get("*", (req, res) => res.sendFile(path.resolve(__dirname + '/dist')))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
+})
 
 app.listen(port);
 console.log('Listen ', port)
