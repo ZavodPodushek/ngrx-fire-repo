9b0ad9a872651672059c3138b4aafcd792675bc8
ZavodPodushek <new_mouse@mail.ru>
2018-06-10 16:02:46 +0300

    Fix all routes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist'));
+app.get("*", (req, res) => res.sendFile(path.resolve(__dirname + '/dist')))
 
 app.listen(port);
 console.log('Listen ', port)
