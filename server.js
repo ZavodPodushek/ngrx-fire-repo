d48cacce102775d8a01cb5c5c58bd514237bd96f
ZavodPodushek <new_mouse@mail.ru>
2018-06-10 14:14:56 +0300

    Fix typo

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', (req, res) =>
-    res.sendFile(path.join(__dirname + '/dist/ngrx-fire-repo/index.html')));
 
 app.listen(port);
 console.log('Listen ', port)
